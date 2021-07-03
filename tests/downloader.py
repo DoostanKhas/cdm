fcda920751e7f18eca04aed7f63ec5d78626e438
Komeil Parseh <ahmdparsh129@gmail.com>
2021-07-03 17:38:45 +0430

    Update downloader.py

--- tests/downloader.py
+++ tests/downloader.py
@@ -1,15 +1,1 @@
 
-from os.path import exists
-
-try:
-    from cdm.downloaders.http import HTTP
-except ImportError as error:
-        print(error.__class__.__name__ + ":" + error.message)
-        exit(0)
-
-def httpTest():
-    
-    http = HTTP(url="https://cdn.realpython.com/static/real-python-logo.893c30edea53.svg")
-    http.download()
-    
-    assert exists(http.getFilename) is True
